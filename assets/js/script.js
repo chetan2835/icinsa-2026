4d561b8ba6ba8f10f393a29a76fb9d24998b6372
chetan2835 <165022718+chetan2835@users.noreply.github.com>
2026-01-07 13:45:27 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,11 @@ setInterval(() => {
   current++;
 }, 2500);
 
+fetch("header.html")
+  .then(res => res.text())
+  .then(data => document.getElementById("header").innerHTML = data);
+
+fetch("footer.html")
+  .then(res => res.text())
+  .then(data => document.getElementById("footer").innerHTML = data);
+
